c7934cd6e265c74f26f4359d9c06954a42b73a39
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2020-02-09 15:43:23 -0800

    Added Tooltip library

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Tooltips
- Simple tooltips which can be injected into pages without conflicts
+
+Simple tooltips which can be injected into pages without conflicts. Tooltips are displayed on hover and will adjust left/right top/bottom to try to fit on the page.
+
+This library is designed to be used in TamperMonkey/GreaseMonkey UserScripts and browser extensions which inject code into other pages. This library has been designed in such a way that it should be compatible and not cause conflicts with the host page regardless of their code. This library is even defined to not conflict with itself so multiple instances of it can be added to a single page and they'll stay separate.
+
+Note: This script is not compatible with older browsers that do not support ES6. For the intended use-case of browser extensions, this should never be an issue.
+
+## Installation
+
+If you would like to add this to a webpage you own, download [Tooltips.js](Tooltips.js) and include the following script tag on your HTML page.
+
+```html
+<script src="Tooltips.js"></script>
+```
+
+If you are using this in a UserScript, copy/paste the code from [Tooltips.min.js](Tooltips.min.js) to the bottom of your script.
+
+## Basic Usage
+
+```js
+const tooltipHelper = new TooltipHelper();
+const elem = document.getElementById("target");
+tooltipHelper.setTooltip(elem, "This is the text you want to display.\n\tThis tooltip is formatted into multiple lines.");
+
+console.log(tooltipHelper.getTooltip(elem)); // Returns the tooltip text
+
+tooltipHelper.setTooltip(elem, "Text can be changed at any time");
+
+tooltipHelper.setTooltip(document.getElementById("another-target"), "You can define as many tooltips as you'd like.");
+
+tooltipHelper.removeTooltip(elem);
+```
+
+## Advanced Usage
+
+```js
+const tooltipHelper = new TooltipHelper({
+    tooltipWidth: YOUR_WIDTH || 300,
+    tooltipHTMLTag: YOUR_CUSTOM_TAG_NAME || "custom-tooltip"
+});
+```
---
+++ Tooltips.js
@@ -0,0 +1,139 @@
+// eslint-disable-next-line no-unused-vars
+class TooltipHelper {
+	constructor(settings) {
+		settings = settings || {}
+		this.settings = {
+			tooltipWidth: settings.tooltipWidth || 300,
+			tooltipHTMLTag: settings.tooltipHTMLTag || "custom-tooltip"
+		};
+		this.tooltipData = {
+			instanceId: Date.now() + Math.random(),
+			cache: new WeakMap(),
+			maxDepth: 0,
+			currentElem: null
+		};
+
+		if (!document.getElementById("tooltip-" + this.tooltipData.instanceId)) {
+			const elem = document.createElement(this.settings.tooltipHTMLTag);
+			elem.id = "tooltip-" + this.tooltipData.instanceId;
+			elem.style.cssText = `
+				background-color: rgba(17,17,17);
+				border-radius: 4px;
+				color: #ffffff;
+				font-size: 13px;
+				font-weight: normal;
+				padding: 0.5em 1em;
+				box-sizing: border-box;
+				z-index: 99999999;
+				font-family: monospace;
+				position: absolute;
+				pointer-events: none;
+				text-align: left;
+				white-space: pre-wrap;
+				width: ${this.settings.tooltipWidth}px;
+				transform: translate3d(0,0,0);
+				-webkit-backface-visibility: hidden;
+				backface-visibility: hidden;
+				opacity: 0;
+				box-shadow: #ffffff 0px 0px 5px 0px inset;
+            `;
+			document.body.appendChild(elem);
+			document.body.addEventListener("mousemove", (evt) => {
+				if (evt.path[0] !== this.tooltipData.currentElem) {
+					this.tooltipData.maxDepth = 0;
+					this.tooltipData.currentElem = null;
+					const tooltip = document.getElementById("tooltip-" + this.tooltipData.instanceId);
+					tooltip.style.opacity = "0";
+					tooltip.style.left = "0";
+					tooltip.style.top = "0";
+				}
+			});
+		}
+	}
+
+	setTooltip(target, text) {
+		let eventListenersAdded = false;
+		if (this.tooltipData.cache.get(target)) {
+			eventListenersAdded = true;
+		}
+		this.tooltipData.cache.set(target, {
+			text: text,
+			mousemove: (evt) => {
+				if (evt.path.length > this.tooltipData.maxDepth) {
+					this.tooltipData.maxDepth = evt.path.length;
+					this.tooltipData.currentElem = target;
+					this._makeTooltipVisible(target);
+				}
+			},
+			mouseleave: () => {
+				if (this.tooltipData.currentElem === target) {
+					this.tooltipData.maxDepth = 0;
+					this.tooltipData.currentElem = null;
+				}
+				const tooltip = document.getElementById("tooltip-" + this.tooltipData.instanceId);
+				tooltip.style.opacity = "0";
+				tooltip.style.left = "0";
+				tooltip.style.top = "0";
+			}
+		});
+		if (eventListenersAdded) {
+			return;
+		}
+		target.addEventListener("mouseenter", this.tooltipData.cache.get(target).mousemove);
+		target.addEventListener("mousemove", this.tooltipData.cache.get(target).mousemove);
+		target.addEventListener("mouseleave", this.tooltipData.cache.get(target).mouseleave);
+	}
+
+	_makeTooltipVisible(target) {
+		const tooltip = document.getElementById("tooltip-" + this.tooltipData.instanceId);
+		tooltip.innerText = this.tooltipData.cache.get(target).text;
+		window.requestAnimationFrame(() => {
+			tooltip.style.opacity = "1";
+			const tooltipRect = tooltip.getBoundingClientRect();
+			const targetRect = this._getAbsoluteBoundingRect(target);
+			if (targetRect.top - tooltipRect.height - 15 > 0) {
+				tooltip.style.top = (targetRect.top - tooltipRect.height - 15) + "px";
+			} else {
+				tooltip.style.top = (targetRect.bottom + 15) + "px";
+			}
+			let tooltipCenter = Math.round(targetRect.left + targetRect.width / 2);
+			if (document.documentElement.scrollWidth > this.settings.tooltipWidth) {
+				if (tooltipCenter + this.settings.tooltipWidth / 2 > document.documentElement.scrollWidth) {
+					tooltipCenter += document.documentElement.scrollWidth - (tooltipCenter + this.settings.tooltipWidth / 2);
+				}
+				if (tooltipCenter - this.settings.tooltipWidth / 2 < 0) {
+					tooltipCenter += 0 - (tooltipCenter - this.settings.tooltipWidth / 2);
+				}
+			}
+			tooltip.style.left = (tooltipCenter - this.settings.tooltipWidth / 2) + "px";
+		});
+	}
+
+	getTooltip(target) {
+		const data = this.tooltipData.cache.get(target);
+		return data ? data.text : null;
+	}
+
+	removeTooltip(target) {
+		if (this.tooltipData.cache.get(target)) {
+			target.removeEventListener("mouseenter", this.tooltipData.cache.get(target).mousemove);
+			target.removeEventListener("mousemove", this.tooltipData.cache.get(target).mousemove);
+			target.removeEventListener("mouseleave", this.tooltipData.cache.get(target).mouseleave);
+			this.tooltipData.cache.delete(target);
+		}
+	}
+
+	_getAbsoluteBoundingRect(elem) {
+		const rect = elem.getBoundingClientRect();
+		const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return {
+			width: rect.width,
+			height: rect.height,
+			top: rect.top + scrollTop,
+			right: rect.right + scrollLeft,
+			bottom: rect.bottom + scrollTop,
+			left: rect.left + scrollLeft
+		};
+	}
+}
